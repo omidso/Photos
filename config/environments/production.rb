e2f267adf4423a4b3ba9dfcadbd24dc1162587bc
Omid <omid.sojoodi@gmail.com>
2013-09-01 14:30:42 -0500

    after precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,23 @@ Photos::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  # config/application.rb
+  config.assets.precompile << Proc.new do |path|
+    if path =~ /\.(css|js)\z/
+      full_path = Rails.application.assets.resolve(path).to_path
+      app_assets_path = Rails.root.join('app', 'assets').to_path
+      if full_path.starts_with? app_assets_path
+        puts "including asset: " + full_path
+        true
+      else
+        puts "excluding asset: " + full_path
+        false
+      end
+    else
+      false
+    end
+  end
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
